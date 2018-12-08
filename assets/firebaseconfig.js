2506c82a21780ddaaca6647c0d7119ecdab2c53d
Jeremy Holthaus <JermJHolt@users.noreply.github.com>
2018-12-08 18:55:37 -0500

    first draft

---
+++ assets/firebaseconfig.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyByEV-qJq6Cr4Cms_s4lJg5wtvKq-My-wY",
+  authDomain: "rockpaperscissors-61fbd.firebaseapp.com",
+  databaseURL: "https://rockpaperscissors-61fbd.firebaseio.com",
+  projectId: "rockpaperscissors-61fbd",
+  storageBucket: "rockpaperscissors-61fbd.appspot.com",
+  messagingSenderId: "586592760360"
+};
+firebase.initializeApp(config);
